b7b5024e4c025a65fc2f1e84329a79b9b66ba3c1
Einscrew <manel.rodrigo.reis@gmail.com>
2018-11-13 23:40:19 +0000

    file download

--- driveTest.py
+++ driveTest.py
@@ -1,6 +1,9 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from pydrive.files import GoogleDriveFile
 from subprocess import Popen, PIPE
+
+from os import mkdir
 # Authenticate the client.
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
@@ -18,25 +21,36 @@ def mimeType(file):
 	else :
 		return 'file'
 
-def inOrder(lvl, id):
+def inOrder(path, id):
 	file_list = drive.ListFile({'q': "'{}' in parents".format(id)}).GetList()
-	for file1 in file_list:
-		tipo=mimeType(file1)
-		print('{} {}: {}'.format(lvl*'\t', tipo ,file1['title']))#, file1['id']))
+	for file in file_list:
+		tipo=mimeType(file)
 		if tipo != 'folder':
-			print('{}Downloading'.format(lvl*'\t'))
-			cmd('touch {}'.format(file1['title']))
+			fname = file['title'].replace(' ', '_')
+			with open('/'.join(path)+'/'+fname, 'wb') as f:
+				try:
+					s = file.GetContentString(mimetype="application/pdf")
+				except:
+					s = file.content.read()
+				f.write(s)
+				
 		else:
-			cmd('mkdir {}'.format(file1['title']))
-			cmd('cd {}'.format(file1['title']))
-			inOrder( lvl + 1 , file1['id'])
+			d = str(file['title']).replace(" ", "_")
+			path.append(d)
+			try:
+				mkdir('/'.join(path))
+			except:
+				pass
+			inOrder( path , file['id'])
 	
-	cmd('cd ../')
+	path.pop()
 
 
 if __name__=="__main__":
-	cmd('mkdir driveTest')
-	cmd('cd driveTest')
+	try:
+		mkdir('driveTest')
+	except:
+		pass
 	#----------vvvvv DriveTest folder id  vvvvvv
-	inOrder(0,"1q7NWIY51qf0k2PNMwZ8Z2kioLV6xquv8")
-	cmd('exit')
+	inOrder(['driveTest'],"1q7NWIY51qf0k2PNMwZ8Z2kioLV6xquv8")
+	
